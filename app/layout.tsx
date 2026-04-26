d0de692bf5abf7201e26766145eb504899c75262
larvrrr-code <larvrrr@gmail.com>
2026-04-26 16:16:45 -0600

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,7 @@ export const metadata: Metadata = {
     'renta segura',
   ],
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/icon.png',
   },
 }
 
